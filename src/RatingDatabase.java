0de3b3c8efb635d007a9f88b38d3396a9c755558
SanaaAbd <sanaaabdulkadir9@gmail.com>
2023-04-18 11:36:44 -0400

    added the book file

--- src/RatingDatabase.java
+++ src/RatingDatabase.java
@@ -1,16 +1,24 @@
-package src;
+
 
 import java.util.HashMap;
 
 public class RatingDatabase{
-
-    private HashMap<int, BookRate> bookRating = new HashMap<int, Bookrating>();
+    private static RatingDatabase instance;
+    private HashMap<Integer, BookRate> bookRatings;
+    public static RatingDatabase getInstance() {
+        if (instance == null) {
+            instance = new RatingDatabase();
+        }
+        return instance;
+    }
 
     public RatingDatabase(){};
 
-    public RatingDatabase getInstance(){}
 
-    public void addBookRating(int bookId, int rating){}
+
+    public void addBookRating(BookRate bookRating) {
+        bookRatings.put(bookRating.getBook(), bookRating);
+    }
 
     public int getBookRating(int bookId){}
 
